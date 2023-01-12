5b6f60a1b8d96f0ab48a20025f7f3ff3e1592703
Sophie Calixto <96028010+sophiecalixto@users.noreply.github.com>
2023-01-12 13:04:05 -0300

    Created second test, returned GREEN
    In the second test, after created (delivery-fee) function, returned GREEN in TDD test

--- src/self_testing_code/core.clj
+++ src/self_testing_code/core.clj
@@ -1,1 +1,8 @@
 (ns self-testing-code.core)
+
+(defn delivery-fee
+  "If the purchase amount is up to 100 reais, a delivery fee of 15 dollars must be charged.
+   If it is greater than 100 and less than 200, 5 dollars.
+   If it is above 200, the fee is free."
+  [purchase-amount]
+  15)
