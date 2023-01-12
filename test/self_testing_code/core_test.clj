4fa8d18b510db42f1e9669804f91856c31f3d23d
Sophie Calixto <96028010+sophiecalixto@users.noreply.github.com>
2023-01-12 12:54:24 -0300

    Created first test, return RED
    In the first TDD test, as expected returns "Unable to resolve symbol: delivery-fee in this context", function not yet implemented.

--- test/self_testing_code/core_test.clj
+++ test/self_testing_code/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [self-testing-code.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+"If the purchase amount is up to 100 reais, a delivery fee of 15 dollars must be charged.\nIf it is greater than 100 and less than 200, 5 dollars.\nIf it is above 200, the fee is free."
+
+(deftest delivery-fee-test
+  (testing "If the value is less than 100, a delivery fee of 15 dollars will be charged"
+    (is (= 15 delivery-fee))))
