4f28784623c4d3eda2cb830339b667d24de5ff47
Sophie Calixto <96028010+sophiecalixto@users.noreply.github.com>
2023-01-12 13:13:02 -0300

    Second test rule returned RED
    After implementing the second test rule, as expected, it returned RED.

--- test/self_testing_code/core_test.clj
+++ test/self_testing_code/core_test.clj
@@ -4,4 +4,6 @@
 
 (deftest delivery-fee-test
   (testing "If the value is less than 100, a delivery fee of 15 dollars will be charged"
-    (is (= 15 (delivery-fee 1)))))
+    (is (= 15 (delivery-fee 1))))
+  (testing "If the purchase amount is between 100.01 and 200, return 5 dollars"
+    (is (= 5 (delivery-fee 100.01)))))
