a921823db39cc459276fc9d64b0a4a60cdf44ab1
Sophie Calixto <96028010+sophiecalixto@users.noreply.github.com>
2023-01-12 12:43:51 -0300

    Initial commit

---
+++ test/self_testing_code/core_test.clj
@@ -0,0 +1,7 @@
+(ns self-testing-code.core-test
+  (:require [clojure.test :refer :all]
+            [self-testing-code.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
